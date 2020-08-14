13a416b1ab3699ee34a9912a7ce83cb04ecce907
Mike Preacher <44718469+mpreacher23@users.noreply.github.com>
2020-08-14 08:53:51 -0500

    updates

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+require("./routes/api-routes")(app)
+require("./routes/html-routes")(app)
+
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
